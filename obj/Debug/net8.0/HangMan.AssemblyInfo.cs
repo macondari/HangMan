548401d3ecbc548dc625bf14d69d3b9f052a2778
macondari <Michaela.Condari@gmail.com>
2024-07-09 17:52:39 -0400

    Added More Words to Dictionary

--- obj/Debug/net8.0/HangMan.AssemblyInfo.cs
+++ obj/Debug/net8.0/HangMan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HangMan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4e8f9c8b93f9361a7cd735761c5f6c9685353e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HangMan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HangMan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
